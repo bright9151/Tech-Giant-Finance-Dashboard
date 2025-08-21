eaeac0098480b3a95d6cbb2661685ca4117fb8ba
Oghenetejiri Bright Uloho <oghenetejiriuloho21@gmail.com>
2025-08-21 14:20:26 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# Config.py : Central place for constants(ickers, start dates, colors, etc.)
+
+# List of companies you want on the dashboard
+COMPANIES = [
+    "Apple",
+    "Microsoft",
+    "Google",
+    "Amazon",
+    "Tesla",
+    "Meta"
+]
+
+# Mapping company names to their stock ticker symbols
+TICKER_MAP = {
+    "Apple": "AAPL",
+    "Microsoft": "MSFT",
+    "Google": "GOOGL",
+    "Amazon": "AMZN",
+    "Tesla": "TSLA",
+    "Meta": "META"
+}
